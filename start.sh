d4f458d99c0a87741b73cf8056fcd52ab6e8dd06
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-06 05:02:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/narutooxox/Rolexv3 /EvaMaria
+  git clone https://github.com/narutooxox/anbesivam.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
-echo "Bot Assembling...."
+echo "Bot Assembling..."
 python3 bot.py
